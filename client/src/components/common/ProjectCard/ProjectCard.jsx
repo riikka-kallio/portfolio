50286eebda5b3a8f3319b1765010cc560f823600
Riikka Kallio <riikka.kal@hotmail.com>
2025-10-12 16:20:17 +0100

    change the image paths

--- client/src/components/common/ProjectCard/ProjectCard.jsx
+++ client/src/components/common/ProjectCard/ProjectCard.jsx
@@ -11,39 +11,39 @@ import IconButton from '@mui/material/IconButton';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkIcon from '@mui/icons-material/Link';
 
-  function ProjectCard({ project }) {
-    return (
-        <Card className="project-card">
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            image={project.image}
-            alt={project.alt}
-          />
-          <Box className="project-box">
-            <Link href={project.githubLink}>
-              <IconButton>
-                  <GitHubIcon className="project-icon"/>
-              </IconButton>
+function ProjectCard({ project }) {
+  return (
+    <Card className="project-card">
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={project.image}
+          alt={project.alt}
+        />
+        <Box className="project-box">
+          <Link href={project.githubLink}>
+            <IconButton>
+              <GitHubIcon className="project-icon" />
+            </IconButton>
           </Link>
           <Link href={project.liveLink}>
-              <IconButton>
-                  <LinkIcon className="project-icon"/>
-              </IconButton>
+            <IconButton>
+              <LinkIcon className="project-icon" />
+            </IconButton>
           </Link>
-          </Box>
-          <CardContent>
-            <Typography variant="h5" component="div" classname="project-title">
-              {project.title}
-            </Typography>
-            <Typography className="project-description">
-              {project.description}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    );
-  }
+        </Box>
+        <CardContent>
+          <Typography variant="h5" component="div" className="project-title">
+            {project.title}
+          </Typography>
+          <Typography className="project-description">
+            {project.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
 
 ProjectCard.propTypes = {
   project: PropTypes.shape({
